e9d5d1ca05549d4250a05c096494148204076ab0
klewy <cigankov807@gmail.com>
2025-11-14 13:09:36 +0300

    Started working on a server class

---
+++ src/server.hpp
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <netinet/in.h>
+#include <print>
+#include <stdexcept>
+#include <sys/socket.h>
+
+
+class Server {
+    int m_sock;
+public:
+
+};
