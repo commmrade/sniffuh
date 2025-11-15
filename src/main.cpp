8a4a106d4878767ff44290f45b8afd5dafb680cd
klewy <cigankov807@gmail.com>
2025-11-15 18:21:16 +0300

    A bit of cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
-#include "utils.hpp"
+#include "logs.hpp"
 #include "server.hpp"
 #include <print>
 
-int main(int argc, char** argv) {
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
     // TODOS:
     // - process VLAN
 
-    auto vec =  show_interfaces();
+    auto vec = show_interfaces();
     if (geteuid() != 0) {
         throw std::runtime_error("Should be ran as root");
     }
