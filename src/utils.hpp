ff19eff1d577a64f037dba1f2c411a3358854be4
klewy <cigankov807@gmail.com>
2025-11-15 18:22:02 +0300

    added utility enums

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,73 +1,12 @@
 #pragma once
 
-#include <cassert>
-#include <chrono>
-#include <cstdint>
-#include <cstdio>
-#include <cstring>
-#include <netinet/in.h>
-#include <print>
-#include <stdexcept>
-#include <sys/socket.h>
-#include <vector>
-#include <sys/types.h>
-#include <ifaddrs.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <net/if.h>
-#include <linux/if_arp.h>
-#include <linux/ip.h>
-#include <linux/tcp.h>
-#include <linux/udp.h>
-#include <sys/ioctl.h>
-
-#define TODO(x) throw std::runtime_error(std::format("TODO: {}", x))
-
-template <typename Action>
-struct defer {
-    Action f_;
-    ~defer() {
-        f_();
-    }
+enum class ethProto {
+    IPv4 = 0x0800,
+    ARP = 0x0806
 };
 
-std::vector<std::string> show_interfaces();
-
-ethhdr parse_eth(std::span<char> bytes);
-
-struct arphdr_f { // ONLY FOR IPv4
-    arphdr hdr;
-    std::vector<char> plod;
+enum class ipProto {
+    TCP = 6,
+    UDP = 17,
+    // So on
 };
-#pragma pack(push, 1)
-struct arphdr_ipv4 {
-    unsigned char		ar_sha[ETH_ALEN];	/* sender hardware address	*/
-	unsigned char		ar_sip[4];		/* sender IP address		*/
-	unsigned char		ar_tha[ETH_ALEN];	/* target hardware address	*/
-	unsigned char		ar_tip[4];		/* target IP address		*/
-};
-#pragma pack(pop)
-
-arphdr_f parse_arp(std::span<char> bytes);
-void print_arp(arphdr_f& arp);
-
-struct iphdr_f {
-	iphdr hdr;
-	std::vector<char> options;
-};
-
-iphdr_f parse_ip(std::span<char> bytes);
-void print_ip(iphdr_f& ip);
-
-
-struct tcphdr_f {
-    tcphdr hdr;
-    std::vector<char> options;
-};
-tcphdr_f parse_tcp(std::span<char> bytes);
-void print_tcp(tcphdr_f& tcp);
-
-udphdr parse_udp(std::span<char> bytes);
-void print_udp(udphdr& udp);
