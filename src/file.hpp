f01e2a346ffaf8f1906fda92af63dce0f0b5ebd7
commmrade <cigankov807@gmail.com>
2025-11-21 02:07:01 +0300

    TODO

--- src/file.hpp
+++ src/file.hpp
@@ -21,14 +21,5 @@ struct Entry { // Big endian for cross platofr
 };
 
 /*
-1. Create a Entry entry;
-2. Pass Entry& to Ethernet handler, it fills shaddr and thaddr
-3. Pass Entry& from Ethernet handler to IP handler and fill saddr and taddr
-4. Pass Entry& from IP Handler to TCP/UDP handler and fill port fields
-
-// But it is kinda tideous, and follows the same chain of resp pattern, therefore it is better to find other ways of doing it, although there are not many
-// Here are all the ways:
-- Do it in a single function which peeks inside Eth, then IP, then TCP/UDP
-- Chain of Responsibility shit
-- Fill Entry in Logs chain of responsibility
+TODO: Write to file as json array
 */
