fc838a169a3eb46711c911148cc7726e2f3e3ed2
klewy <cigankov807@gmail.com>
2025-11-26 08:52:45 +0300

    Finished filters and cleaned up the main

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include <exception>
 #include <netinet/in.h>
 #include <print>
+#include <ranges>
 #include <stdexcept>
 #include <string>
 #include "argparse/argparse.hpp"
 #include "utils.hpp"
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
-    argparse::ArgumentParser parser;
+void setup_parser(argparse::ArgumentParser& parser) {
     {
         auto& group = parser.add_mutually_exclusive_group(true);
         group.add_argument("--log")
@@ -22,17 +22,9 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
         group.add_argument("--interfaces")
             .flag()
             .help("Specify which interface you want to 'sniff'");
-
-        // TODO: Show interfaces option
     }
     {
         auto& w_group = parser.add_group("Write options");
-        w_group.add_argument("--opt1")
-            .default_value(std::string{"Def"})
-            .help("Test option");
-        w_group.add_argument("--opt2")
-            .default_value(std::string{"Def"})
-            .help("Test option");
         w_group.add_argument("--interface")
             .default_value(std::string{ANY_INTERFACE})
             .help("Specify an interface");
@@ -72,7 +64,146 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
             .help("Find records for the specified target port");
         // Setup filters and shit
     }
+}
 
+void log_mode(argparse::ArgumentParser& parser) {
+    if (geteuid() != 0) {
+        throw std::runtime_error("Should be ran as root");
+    }
+
+    LogLevel loglvl;
+    int lvl = parser.get<int>("--log-level");
+    if (lvl < 0) {
+        std::println("Log level can't be set to {}, set to 0 (min)", lvl);
+    } else if (lvl > 2) {
+        std::println("Log level can't be set to {}, set to 2 (max)", lvl);
+    }
+    loglvl = static_cast<LogLevel>(lvl);
+
+    std::string ifc = parser.get<std::string>("--interface");
+
+    Sniffer s{ifc};
+    s.set_log_lvl(loglvl);
+    s.sniff_loop();
+}
+
+void read_mode(argparse::ArgumentParser& parser) {
+    auto entries = read_file("test.json");
+
+    if (parser.is_used("time-order")) {
+        int order_val = parser.get<int>("time-order");
+        if (order_val < 0 || order_val > 1) {
+            throw std::runtime_error("Invalid value for time order option");
+        }
+        std::ranges::sort(entries, [order_val](const Entry& lhs, const Entry& rhs) {
+            return order_val == 0 ? lhs.ts < rhs.ts : lhs.ts > rhs.ts;
+        });
+    }
+
+    if (parser.is_used("shaddr")) {
+        auto res = convert_to_mac(parser.get<std::string>("shaddr"));
+        auto r = res.and_then([&entries](std::array<char, 6> raddr) -> std::expected<std::array<char, 6>, std::string> {
+            auto filtered = std::views::filter(entries, [raddr](const Entry& en) {
+                return !std::memcmp(raddr.data(), en.shaddr.data(), raddr.size());
+            }) | std::ranges::to<std::vector<Entry>>();
+            entries = std::move(filtered);
+            return {};
+        }).or_else([](const std::string& err) -> std::expected<std::array<char, 6>, std::string> {
+            throw std::runtime_error(err);
+            return {};
+        });
+    }
+    if (parser.is_used("thaddr")) {
+        auto res = convert_to_mac(parser.get<std::string>("thaddr"));
+        auto r = res.and_then([&entries](std::array<char, 6> raddr) -> std::expected<std::array<char, 6>, std::string> {
+            auto filtered = std::views::filter(entries, [raddr](const Entry& en) {
+                return !std::memcmp(raddr.data(), en.thaddr.data(), raddr.size());
+            }) | std::ranges::to<std::vector<Entry>>();
+            entries = std::move(filtered);
+            return {};
+        }).or_else([](const std::string& err) -> std::expected<std::array<char, 6>, std::string> {
+            throw std::runtime_error(err);
+            return {};
+        });
+    }
+    if (parser.is_used("eth-proto")) {
+        auto proto = parser.get<std::uint32_t>("eth-proto");
+        auto filtered = std::views::filter(entries, [proto](const Entry& en) {
+            return ntohl(en.eth_proto) == proto;
+        }) | std::ranges::to<std::vector<Entry>>();
+        entries = std::move(filtered);
+    }
+    if (parser.is_used("saddr")) {
+        auto addr = convert_to_addr(parser.get<std::string>("saddr"));
+        auto r = addr.and_then([&entries](in_addr_t addr) ->  std::expected<in_addr_t, std::string> {
+            auto filtered = entries | std::views::filter([addr](const Entry& en){
+                uint32_t ip;
+                std::memcpy(&ip, en.saddr.data(), 4);
+                return ip == addr;
+            }) | std::ranges::to<std::vector>();
+            entries = std::move(filtered);
+            return {};
+        }).or_else([](const std::string& err) -> std::expected<in_addr_t, std::string> {
+            throw std::runtime_error(std::format("Failed to filter by saddr: {}", err));
+            return {};
+        });
+    }
+    if (parser.is_used("taddr")) {
+        auto addr = convert_to_addr(parser.get<std::string>("taddr"));
+        auto r = addr.and_then([&entries](in_addr_t addr) ->  std::expected<in_addr_t, std::string> {
+            auto filtered = entries | std::views::filter([addr](const Entry& en){
+                uint32_t ip;
+                std::memcpy(&ip, en.taddr.data(), 4);
+                return ip == addr;
+            }) | std::ranges::to<std::vector>();
+            entries = std::move(filtered);
+            return {};
+        }).or_else([](const std::string& err) -> std::expected<in_addr_t, std::string> {
+            throw std::runtime_error(std::format("Failed to filter by taddr: {}", err));
+            return {};
+        });
+    }
+    if (parser.is_used("ip-proto")) {
+        int proto = parser.get<int>("ip-proto");
+
+        auto filtered = entries | std::views::filter([proto](const Entry& en){
+            return en.ip_proto == proto;
+        }) | std::ranges::to<std::vector>();
+        entries = std::move(filtered);
+    }
+    if (parser.is_used("sport")) {
+        std::uint16_t port = htons(parser.get<std::uint16_t>("sport")); // to be, since port is stored in be
+
+        auto filtered = entries | std::views::filter([port](const Entry& en){
+            return en.sport == port;
+        }) | std::ranges::to<std::vector>();
+        entries = std::move(filtered);
+    }
+    if (parser.is_used("tport")) {
+        std::uint16_t port = htons(parser.get<std::uint16_t>("tport"));
+
+        auto filtered = entries | std::views::filter([port](const Entry& en){
+            return en.tport == port;
+        }) | std::ranges::to<std::vector>();
+        entries = std::move(filtered);
+    }
+
+    for (const auto& en : entries) {
+        print_entry(en);
+    }
+}
+
+void interfaces_mode() {
+    auto ifcs = show_interfaces();
+    for (const auto& ifc : ifcs) {
+        std::println("Interface: {}", ifc);
+    }
+}
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    argparse::ArgumentParser parser;
+    setup_parser(parser);
 
     try {
         parser.parse_args(argc, argv);
@@ -82,149 +213,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
     }
 
     if (parser.is_used("log")) {
-        if (geteuid() != 0) {
-            throw std::runtime_error("Should be ran as root");
-        }
-
-        LogLevel loglvl;
-        int lvl = parser.get<int>("--log-level");
-        if (lvl < 0) {
-            std::println("Log level can't be set to {}, set to 0 (min)", lvl);
-        } else if (lvl > 2) {
-            std::println("Log level can't be set to {}, set to 2 (max)", lvl);
-        }
-        loglvl = static_cast<LogLevel>(lvl);
-
-        std::string ifc = parser.get<std::string>("--interface");
-
-        Sniffer s{ifc};
-        s.set_log_lvl(loglvl);
-        s.sniff_loop();
+        log_mode(parser);
     } else if (parser.is_used("read")) {
-        auto entries = read_file("test.json");
-
-
-        if (parser.is_used("time-order")) {
-            int order_val = parser.get<int>("time-order");
-            if (order_val < 0 || order_val > 1) {
-                throw std::runtime_error("Invalid value for time order option");
-            }
-            std::ranges::sort(entries, [order_val](const Entry& lhs, const Entry& rhs) {
-                return order_val == 0 ? lhs.ts < rhs.ts : lhs.ts > rhs.ts;
-            });
-        }
-        if (parser.is_used("shaddr")) {
-            std::string addr = parser.get<std::string>("shaddr"); // get addr like 25:12:4a:11:09:2f
-            std::array<char, 6> raddr{};
-
-            auto mac = std::views::split(addr, ':') | std::ranges::to<std::vector<std::string>>();
-            if (mac.size() != 6) {
-                throw std::runtime_error("Source hardware address is invalid");
-            }
-
-            for (std::size_t i = 0; i < raddr.size(); ++i) {
-                raddr[i] = std::stoi(mac[i], nullptr, 16);
-            }
-
-            auto filtered = std::views::filter(entries, [raddr](const Entry& en) {
-                return !std::memcmp(raddr.data(), en.shaddr.data(), raddr.size());
-            }) | std::ranges::to<std::vector<Entry>>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("thaddr")) {
-            std::string addr = parser.get<std::string>("thaddr"); // get addr like 25:12:4a:11:09:2f
-            std::array<char, 6> raddr{};
-
-            auto mac = std::views::split(addr, ':') | std::ranges::to<std::vector<std::string>>();
-            if (mac.size() != 6) {
-                throw std::runtime_error("Target hardware address is invalid");
-            }
-
-            for (std::size_t i = 0; i < raddr.size(); ++i) {
-                raddr[i] = std::stoi(mac[i], nullptr, 16);
-            }
-
-            auto filtered = std::views::filter(entries, [raddr](const Entry& en) {
-                return !std::memcmp(raddr.data(), en.thaddr.data(), raddr.size());
-            }) | std::ranges::to<std::vector<Entry>>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("eth-proto")) {
-            auto proto = parser.get<std::uint32_t>("eth-proto");
-            auto filtered = std::views::filter(entries, [proto](const Entry& en) {
-                return ntohl(en.eth_proto) == proto;
-            }) | std::ranges::to<std::vector<Entry>>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("saddr")) {
-            std::string addr_str = parser.get<std::string>("saddr");
-            in_addr_t addr{};
-
-            int r = inet_pton(AF_INET, addr_str.c_str(), &addr);
-            if (r != 1) {
-                throw std::runtime_error("Source address is invalid");
-            }
-
-            auto filtered = entries | std::views::filter([addr](const Entry& en){
-                uint32_t ip;
-                std::memcpy(&ip, en.saddr.data(), 4);
-                return ip == addr;
-            }) | std::ranges::to<std::vector>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("taddr")) {
-            std::string addr_str = parser.get<std::string>("taddr");
-            in_addr_t addr{};
-
-            int r = inet_pton(AF_INET, addr_str.c_str(), &addr);
-            if (r != 1) {
-                throw std::runtime_error("Target address is invalid");
-            }
-
-            auto filtered = entries | std::views::filter([addr](const Entry& en){
-                uint32_t ip;
-                std::memcpy(&ip, en.taddr.data(), 4);
-                return ip == addr;
-            }) | std::ranges::to<std::vector>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("ip-proto")) {
-            int proto = parser.get<int>("ip-proto");
-
-            auto filtered = entries | std::views::filter([proto](const Entry& en){
-                return en.ip_proto == proto;
-            }) | std::ranges::to<std::vector>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("sport")) {
-            std::uint16_t port = htons(parser.get<std::uint16_t>("sport")); // to be, since port is stored in be
-
-            auto filtered = entries | std::views::filter([port](const Entry& en){
-                return en.sport == port;
-            }) | std::ranges::to<std::vector>();
-            entries = std::move(filtered);
-        }
-        if (parser.is_used("tport")) {
-            std::uint16_t port = htons(parser.get<std::uint16_t>("tport"));
-
-            auto filtered = entries | std::views::filter([port](const Entry& en){
-                return en.tport == port;
-            }) | std::ranges::to<std::vector>();
-            entries = std::move(filtered);
-        }
-
-        for (const auto& en : entries) {
-            // std::println("{}", en.ts);
-            print_entry(en);
-        }
-
-
-
+        read_mode(parser);
     } else if (parser.is_used("interfaces")) {
-        auto ifcs = show_interfaces();
-        for (const auto& ifc : ifcs) {
-            std::println("Interface: {}", ifc);
-        }
+        interfaces_mode();
     }
     return 0;
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include <expected>
+#include <netinet/in.h>
 #include <string>
 #include <vector>
+#include <array>
 
 template <typename Action>
 struct defer {
@@ -12,3 +15,6 @@ struct defer {
 
 constexpr std::string_view ANY_INTERFACE = "any";
 std::vector<std::string> show_interfaces();
+
+std::expected<in_addr_t, std::string> convert_to_addr(const std::string& addr_str);
+std::expected<std::array<char, 6>, std::string> convert_to_mac(const std::string& mac_str);
